32455f452063ac23dbb41aac1c820f2912feb67a
Jon Fernandez <jonfernandez@macbookpro.speedport.ip>
2025-02-05 09:23:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # leetcode_solutions
+
+A small repository that will keep all the leetcode solutions I have been
+working on and its solutions.
+
+Also, I will be running Github Actions to run pytest and static linting tests
+on them to practice CI/CD. 
---
+++ merge_sorted_arrays.py
@@ -0,0 +1,73 @@
+
+import pytest 
+
+'''
+You are given two integer arrays nums1 and nums2, 
+sorted in non-decreasing order, and two integers m and n,
+representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, 
+but instead be stored inside the array nums1. 
+To accommodate this, nums1 has a length of m + n, 
+where the first m elements denote the elements that should be merged, 
+and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+'''
+
+def merge_sorted_arrays(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    '''
+    Time complexity: O(m+n)
+    Space complexity: O(1)
+    '''
+
+    nums1index = m - 1
+    nums2index= n - 1 
+    nums1endindex = (m + m) - 1
+
+    while nums2index >= 0:
+        if nums1[nums1index] > nums2[nums2index] and nums1index >= 0:
+            nums1[nums1endindex] = nums1[nums1index]
+            nums1index -= 1
+        else:
+            nums1[nums1endindex] = nums2[nums2index]
+            nums2index -= 1
+        nums1endindex -= 1
+
+
+@pytest.mark.parametrize(('nums1, nums2, result'), [
+        ([1,2,5,0,0,0], [8,9,10], [1,2,5,8,9,10]),
+        ([4,5,6,0,0,0], [1,2,3], [1,2,3,4,5,6]),
+        ([7,8,9,0,0,0], [2,5,6], [2,5,6,7,8,9]),])
+def test_merged_sorted_arrays(nums1, nums2, result):
+    merge_sorted_arrays(nums1, 3, nums2, 3)
+    assert nums1 == result
+
