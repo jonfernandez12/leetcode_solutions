f2b60674b92a51b5e2792e3b13f4b5d74342931c
jonfernandez12 <jonfernandez@macbookpro.speedport.ip>
2025-02-12 15:54:33 +0100

    Add rotate array challenge

---
+++ tests/test_rotate_array.py
@@ -0,0 +1,14 @@
+import pytest
+from utils.rotate_array import rotate
+
+
+@pytest.mark.parametrize(
+    'nums, k, result',
+    [
+        ([1, 2, 3, 4, 5, 6, 7], 2, [6, 7, 1, 2, 3, 4, 5]),
+        ([-1, -100, 3, 99], 2, [3, 99, -1, -100]),
+    ],
+)
+def test_rotate(nums, k, result):
+    rotate(nums, k)
+    assert nums == result
